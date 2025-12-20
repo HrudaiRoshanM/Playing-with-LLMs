4eeab326c41abe704c1e6f3b06c998d907f53889
Hrudai Roshan Mekala <hrudairoshanmekala@gmail.com>
2025-12-21 00:56:11 +0530

    Update ModelConfig and refine tensor operations

--- Nano GPT-OSS/gptoss.py
+++ Nano GPT-OSS/gptoss.py
@@ -6,33 +6,39 @@ from dataclasses import dataclass
 import torch
 import torch.distributed as dist
 
+
+
+
 @dataclass
 class ModelConfig:
-    num_hidden_layers: int = 12 # no of transformer blocks
-    hidden_size: int = 2880 # size of token embedding
-    head_dim: int = 64 # dimensionality of each attention head
-    num_attention_heads: int = 64 # total number of query heads
-    num_key_value_heads: int = 8 # no of key/value heads
+    num_hidden_layers: int = 24
+    num_experts: int = 32
+    experts_per_token: int = 4
+    vocab_size: int = 201088
+    hidden_size: int = 2880
+    intermediate_size: int = 2880
+    swiglu_limit: float = 7.0
+    head_dim: int = 64
+    num_attention_heads: int = 64
+    num_key_value_heads: int = 8
     sliding_window: int = 128
-    rope_theta: float = 150000.0
     initial_context_length: int = 4096
+    rope_theta: float = 150000.0
     rope_scaling_factor: float = 32.0
     rope_ntk_alpha: float = 1.0
     rope_ntk_beta: float = 32.0
-    num_experts: int = 32
-    experts_per_token: int = 4
-    intermediate_size: int = 2880 #In the feed forward network, when network is expanded hidden_size
-    swiglu_limit: float = 7.0
 
 
 class RMSNorm(torch.nn.Module):
     def __init__(
-        self, num_features: int, eps: float = 1e-5, device: torch.device | None = None
+        self, num_features: int, eps: float = 1e-05, device: torch.device | None = None
     ):
         super().__init__()
         self.num_features = num_features
         self.eps = eps
-        self.scale = torch.nn.Parameter(torch.ones, device=device, dtype=torch.float32)
+        self.scale = torch.nn.Parameter(
+            torch.ones(num_features, device=device, dtype=torch.float32)
+        )
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         assert x.shape[-1] == self.num_features
@@ -41,18 +47,18 @@ class RMSNorm(torch.nn.Module):
         return (t * self.scale).to(dtype)
 
 
-
 def _apply_rotary_emb(
-      x: torch.Tensor,
-      cos: torch.Tensor,
-      sin: torch.Tensor,
-  ) -> torch.Tensor:
-      cos = cos.unsqueeze(-2).to(x.dtype)
-      sin = sin.unsqueeze(-2).to(x.dtype)
-      x1, x2 = torch.chunk(x, 2, dim=-1)
-      o1 = x1 * cos - x2 * sin
-      o2 = x1 * sin + x2 * cos
-      return torch.cat((o1, o2), dim=-1)
+    x: torch.Tensor,
+    cos: torch.Tensor,
+    sin: torch.Tensor,
+) -> torch.Tensor:
+    cos = cos.unsqueeze(-2).to(x.dtype)
+    sin = sin.unsqueeze(-2).to(x.dtype)
+    x1, x2 = torch.chunk(x, 2, dim=-1)
+    o1 = x1 * cos - x2 * sin
+    o2 = x2 * cos + x1 * sin
+    return torch.cat((o1, o2), dim=-1)
+
 
 class RotaryEmbedding(torch.nn.Module):
     def __init__(
@@ -77,81 +83,90 @@ class RotaryEmbedding(torch.nn.Module):
         self.device = device
 
     def _compute_concentration_and_inv_freq(self) -> torch.Tensor:
-        freq = self.base ** ((torch.arange(0, self.head_dim, 2, dtype=torch.float, device=self.device))/ self.head_dim) #inverse theta
-        if self.scaling_factor >1:
-            concentration = (0.1* math.log(self.scaling_factor) + 1.0) #concentration factor
-            d_half = self.head_dim/2
+        """See YaRN paper: https://arxiv.org/abs/2309.00071"""
+        freq = self.base ** (
+            torch.arange(0, self.head_dim, 2, dtype=torch.float, device=self.device)
+            / self.head_dim
+        )
+        if self.scaling_factor > 1.0:
+            concentration = (
+                0.1 * math.log(self.scaling_factor) + 1.0
+            )  # YaRN concentration
+
+            d_half = self.head_dim / 2
+            # NTK by parts
             low = (
                 d_half
-                * math.log(self.initial_context_length/ (self.ntk_beta * 2 * math.pi))
+                * math.log(self.initial_context_length / (self.ntk_beta * 2 * math.pi))
                 / math.log(self.base)
             )
             high = (
                 d_half
-                * math.log(self.initial_context_length/ (self.ntk_alpha * 2 * math.pi))
+                * math.log(self.initial_context_length / (self.ntk_alpha * 2 * math.pi))
                 / math.log(self.base)
             )
             assert 0 < low < high < d_half - 1
 
-            interpolation = 1.0/ (self.scaling_factor * freq)
-            extrapolation = 1.0/ freq
+            interpolation = 1.0 / (self.scaling_factor * freq)
+            extrapolation = 1.0 / freq
+
             ramp = (
                 torch.arange(d_half, dtype=torch.float32, device=freq.device) - low
             ) / (high - low)
             mask = 1 - ramp.clamp(0, 1)
 
-            inv_freq = interpolation * (1-mask) + extrapolation * mask
-
+            inv_freq = interpolation * (1 - mask) + extrapolation * mask
         else:
-            concentration = 1
-            inv_freq = 1.0/freq
+            concentration = 1.0
+            inv_freq = 1.0 / freq
 
         return concentration, inv_freq
 
     def _compute_cos_sin(self, num_tokens: int):
         concentration, inv_freq = self._compute_concentration_and_inv_freq()
-        t = torch.arange(num_tokens, dtype=torch.float, device=self.device)
-        freqs = torch.einsum("i,j->ij",t, inv_freq)
-        cos = torch.cos(freqs)
-        sin = torch.sin(freqs)
+        t = torch.arange(num_tokens, dtype=torch.float32, device=self.device)
+        freqs = torch.einsum("i,j->ij", t, inv_freq)
+        cos = freqs.cos() * concentration
+        sin = freqs.sin() * concentration
         return cos, sin
 
-    def forward(self, query:torch.Tensor, key:torch.Tensor) -> tuple[torch.Tensor, torch.Tensor]:
+    def forward(
+        self,
+        query: torch.Tensor,
+        key: torch.Tensor,
+    ) -> tuple[torch.Tensor, torch.Tensor]:
         num_tokens = query.shape[0]
         cos, sin = self._compute_cos_sin(num_tokens)
 
         query_shape = query.shape
         query = query.view(num_tokens, -1, self.head_dim)
         query = _apply_rotary_emb(query, cos, sin)
-        query = query.view(query_shape)
+        query = query.reshape(query_shape)
 
         key_shape = key.shape
         key = key.view(num_tokens, -1, self.head_dim)
         key = _apply_rotary_emb(key, cos, sin)
-        key = key.view(key_shape)
+        key = key.reshape(key_shape)
         return query, key
 
 
 def sdpa(Q, K, V, S, sm_scale, sliding_window=0):
-    #scaled dot product attention
-    #We are having GroupedQueryAttention + AttentionBias + SlidingWindowAttention
-    #sm here mean softmax and sm_scale means softmax scaling
+    # sliding_window == 0 means no sliding window
     n_tokens, n_heads, q_mult, d_head = Q.shape
     assert K.shape == (n_tokens, n_heads, d_head)
     assert V.shape == (n_tokens, n_heads, d_head)
     K = K[:, :, None, :].expand(-1, -1, q_mult, -1)
     V = V[:, :, None, :].expand(-1, -1, q_mult, -1)
     S = S.reshape(n_heads, q_mult, 1, 1).expand(-1, -1, n_tokens, -1)
-    #S initially has the single dimension with values n_heads*q_mult, one sink value query head
     mask = torch.triu(Q.new_full((n_tokens, n_tokens), -float("inf")), diagonal=1)
     if sliding_window > 0:
         mask += torch.tril(
-            mask.new_full((n_tokens, n_tokens), -float("inf")), diagonal= -sliding_window
+            mask.new_full((n_tokens, n_tokens), -float("inf")), diagonal=-sliding_window
         )
     QK = torch.einsum("qhmd,khmd->hmqk", Q, K)
     QK *= sm_scale
     QK += mask[None, None, :, :]
-    QK = torch.cat((QK,S), dim=-1)
+    QK = torch.cat([QK, S], dim=-1)
     W = torch.softmax(QK, dim=-1)
     W = W[..., :-1]
     attn = torch.einsum("hmqk,khmd->qhmd", W, V)
@@ -162,7 +177,7 @@ class AttentionBlock(torch.nn.Module):
     def __init__(
         self,
         config: ModelConfig,
-        layer_idx: int=0,
+        layer_idx: int = 0,
         device: torch.device | None = None,
     ):
         super().__init__()
@@ -175,24 +190,28 @@ class AttentionBlock(torch.nn.Module):
             torch.empty(config.num_attention_heads, device=device, dtype=torch.bfloat16)
         )
         self.norm = RMSNorm(config.hidden_size, device=device)
-        qkv_dim = config.head_dim * (config.num_attention_heads+ 2 * config.num_key_value_heads)
-        self.qkv = torch.nn.Linear(config.hidden_size, qkv_dim, device=device, dtype=torch.bfloat16)
+        qkv_dim = config.head_dim * (
+            config.num_attention_heads + 2 * config.num_key_value_heads
+        )
+        self.qkv = torch.nn.Linear(
+            config.hidden_size, qkv_dim, device=device, dtype=torch.bfloat16
+        )
         self.out = torch.nn.Linear(
             config.head_dim * config.num_attention_heads,
             config.hidden_size,
             device=device,
             dtype=torch.bfloat16,
         )
-        self.sm_scale = 1.0 / math.sqrt(config.head_dim)
+        self.sm_scale = 1 / math.sqrt(config.head_dim)
         self.rope = RotaryEmbedding(
             config.head_dim,
             config.rope_theta,
             torch.float32,
-            initial_context_length = config.initial_context_length
-            scaling_factor = config.rope_scaling_factor,
-            ntk_alpha = config.rope_ntk_alpha,
-            ntk_beta = config.rope_ntk_beta,
-            device = device
+            initial_context_length=config.initial_context_length,
+            scaling_factor=config.rope_scaling_factor,
+            ntk_alpha=config.rope_ntk_alpha,
+            ntk_beta=config.rope_ntk_beta,
+            device=device,
         )
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
@@ -203,12 +222,15 @@ class AttentionBlock(torch.nn.Module):
             :,
             self.num_attention_heads
             * self.head_dim : (self.num_attention_heads + self.num_key_value_heads)
-            * self.head_dim].contiguous()
+            * self.head_dim,
+        ].contiguous()
         v = qkv[
             :,
             (self.num_attention_heads + self.num_key_value_heads)
             * self.head_dim : (self.num_attention_heads + 2 * self.num_key_value_heads)
-            * self.head_dim].contiguous()
+            * self.head_dim,
+        ].contiguous()
+
         q = q.view(
             -1,
             self.num_key_value_heads,
@@ -217,22 +239,24 @@ class AttentionBlock(torch.nn.Module):
         )
         k = k.view(-1, self.num_key_value_heads, self.head_dim)
         v = v.view(-1, self.num_key_value_heads, self.head_dim)
-
         q, k = self.rope(q, k)
         t = sdpa(q, k, v, self.sinks, self.sm_scale, self.sliding_window)
         t = self.out(t)
         t = x + t
         return t
 
+
 def swiglu(x, alpha: float = 1.702, limit: float = 7.0):
     x_glu, x_linear = x[..., ::2], x[..., 1::2]
-    #clamp the input values
+    # Clamp the input values
     x_glu = x_glu.clamp(min=None, max=limit)
     x_linear = x_linear.clamp(min=-limit, max=limit)
     out_glu = x_glu * torch.sigmoid(alpha * x_glu)
+    # Note we add an extra bias of 1 to the linear layer
     return out_glu * (x_linear + 1)
 
-class MLPB(torch.nn.Module): #Multi-layer perceptron block
+class MLPBlock(torch.nn.Module):
+
     def __init__(
         self,
         config: ModelConfig,
@@ -245,80 +269,72 @@ class MLPB(torch.nn.Module): #Multi-layer perceptron block
         self.world_size = dist.get_world_size() if dist.is_initialized() else 1
         self.norm = RMSNorm(config.hidden_size, device=device)
         self.gate = torch.nn.Linear(
-            config.hidden_size,
-            config.num_experts,
-            device = device,
-            dtype = torch.bfloat16
+            config.hidden_size, config.num_experts, device=device, dtype=torch.bfloat16
         )
         assert config.intermediate_size % self.world_size == 0
+        
         # Store experts as a list of separate modules to avoid indexing issues
-
         self.experts = torch.nn.ModuleList([
             torch.nn.Sequential(
                 torch.nn.Linear(
-                    config.hidden_size,
-                    config.intermediate_size * 2 // self.world_size,
-                    device = device,
-                    dtype = torch.bfloat16,
+                    config.hidden_size, 
+                    config.intermediate_size * 2 // self.world_size, 
+                    device=device, 
+                    dtype=torch.bfloat16
                 ),
                 torch.nn.Linear(
-                    config.intermediate_size // self.world_size,
-                    config.hidden_size,
-                    device = device,
-                    dtype = torch.bfloat16
+                    config.intermediate_size // self.world_size, 
+                    config.hidden_size, 
+                    device=device, 
+                    dtype=torch.bfloat16
                 )
-            ) for _ in range(self.num_experts)
+            ) for _ in range(config.num_experts)
         ])
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         seq_len, hidden_size = x.shape
         t = self.norm(x)
         g = self.gate(t)
-
-        # Taking top-k experts
+        
+        # Get top-k experts
         experts = torch.topk(g, k=self.experts_per_token, dim=-1, sorted=True)
         expert_weights = torch.nn.functional.softmax(experts.values, dim=-1)
         expert_indices = experts.indices
-
-        # flatenning the process
+        
+        # Flatten for processing
         t_flat = t.view(-1, hidden_size)
         expert_indices_flat = expert_indices.view(-1, self.experts_per_token)
         expert_weights_flat = expert_weights.view(-1, self.experts_per_token)
-
+        
         output = torch.zeros_like(t_flat)
-
+        
         # Process each expert
         for expert_idx in range(self.num_experts):
             mask = (expert_indices_flat == expert_idx).any(dim=-1)
-            # .any() returns the True value, along the last dimension
             if not mask.any():
                 continue
-
-            token_indices = torch.where(mask)[0] # .where() returns indices where mask is True
-            # Hence token_indices is tuple with indices indicating, expert numbers for that token
-
+                
+            token_indices = torch.where(mask)[0]
             expert_pos = (expert_indices_flat[token_indices] == expert_idx).nonzero(as_tuple=True)[1]
-            # It is returning token indices which are going inside expert for each expert_idx
-
+            
             expert_input = t_flat[token_indices]
-            # normalized token embeddings which will go to expert in the for loop
-
             weights = expert_weights_flat[token_indices, expert_pos]
-
+            
             # Forward through this expert
             expert_out = expert_input
-
-            expert_out = self.experts[expert_idx][0](expert_out) # 1st linear + activation
+            expert_out = self.experts[expert_idx][0](expert_out)  # First linear + activation
             expert_out = swiglu(expert_out, limit=self.swiglu_limit)
-            expert_out = self.experts[expert_idx][1](expert_out) # Second linear
-
+            expert_out = self.experts[expert_idx][1](expert_out)  # Second linear
+            
             output[token_indices] += expert_out * weights.unsqueeze(-1)
-
+        
         if self.world_size > 1:
             dist.all_reduce(output, op=dist.ReduceOp.SUM)
-
+        
         output = output.view(seq_len, hidden_size)
         return x + output
+
+
 
 
 class TransformerBlock(torch.nn.Module):
@@ -331,12 +347,13 @@ class TransformerBlock(torch.nn.Module):
         super().__init__()
         self.layer_idx = layer_idx
         self.attn = AttentionBlock(config, layer_idx, device)
-        self.mlp = MLPB(config, device)
+        self.mlp = MLPBlock(config, device)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         x = self.attn(x)
         x = self.mlp(x)
         return x
+
 
 class Transformer(torch.nn.Module):
     def __init__(
@@ -345,8 +362,9 @@ class Transformer(torch.nn.Module):
         device: torch.device | None = None,
     ):
         super().__init__()
-        self.embedding = torch.nn.Embedding(config.vocab_size, config.hidden_size,
-                                            device=device, dytype=torch.bfloat16)
+        self.embedding = torch.nn.Embedding(
+            config.vocab_size, config.hidden_size, device=device, dtype=torch.bfloat16
+        )
         self.block = torch.nn.ModuleList(
             [
                 TransformerBlock(config, layer_idx, device)
@@ -357,7 +375,7 @@ class Transformer(torch.nn.Module):
         self.unembedding = torch.nn.Linear(
             config.hidden_size,
             config.vocab_size,
-            bias = False,
+            bias=False,
             device=device,
             dtype=torch.bfloat16,
         )
@@ -370,12 +388,10 @@ class Transformer(torch.nn.Module):
         x = self.unembedding(x)
         return x
 
-
     @staticmethod
     def from_checkpoint(
         path: str, device: str | torch.device = "cuda"
     ) -> "Transformer":
-    # isinstance is used to check, whether object(device) belongs to a particular class
         if not isinstance(device, torch.device):
             device = torch.device(device)
 
@@ -434,57 +450,29 @@ class TokenGenerator:
 
     @torch.inference_mode()
     def generate(self,
-                 promt_tokens: list[int],
+                 prompt_tokens: list[int],
                  stop_tokens: list[int],
                  temperature: float = 1.0,
                  max_tokens: int = 0,
                  return_logprobs: bool = False):
-        tokens = list(promt_tokens)
+        tokens = list(prompt_tokens)
         num_generated_tokens = 0
         while max_tokens == 0 or num_generated_tokens < max_tokens:
-            logits = self.model(torch.tensor(tokens, dtype=torch.int32, device=self.device))[-1]
-            if temperature == 0:
+            logits = self.model(torch.as_tensor(tokens, dtype=torch.int32, device=self.device))[-1]
+            if temperature == 0.0:
                 predicted_token = torch.argmax(logits, dim=-1).item()
             else:
-                probs = torch.softmax(logits / temperature, dim=-1)
+                probs = torch.softmax(logits * (1.0 / temperature), dim=-1)
                 predicted_token = torch.multinomial(probs, num_samples=1).item()
             tokens.append(predicted_token)
             num_generated_tokens += 1
 
             if return_logprobs:
                 logprobs = torch.log_softmax(logits, dim=-1)
-                selected_logprobs = logprobs[predited_token].item()
+                selected_logprobs = logprobs[predicted_token].item()
                 yield predicted_token, selected_logprobs
             else:
                 yield predicted_token
 
             if predicted_token in stop_tokens:
                 break
-
-
-
-
-            
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
